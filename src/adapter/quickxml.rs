602acb59c51bb2f21a41c73d6229d8523f55bcd8
Andrei Vasiliu <vasiliua@amazon.com>
2019-09-07 15:52:27 +0300

    Update to quick-xml 0.16

--- src/adapter/quickxml.rs
+++ src/adapter/quickxml.rs
@@ -5,6 +5,8 @@ use super::*;
 use std::io::BufRead;
 use error::{ErrorKind, Result};
 
+// FIXME: unwraps should be proper errors
+
 impl<R: BufRead> GenericEventReader for Reader<R> {
     fn next(&mut self) -> Result<GenericXmlEvent> {
         loop {
@@ -15,16 +17,16 @@ impl<R: BufRead> GenericEventReader for Reader<R> {
             return match event {
                 Event::Eof => Ok(GenericXmlEvent::EndDocument),
                 Event::Start(bytes_start) => Ok(GenericXmlEvent::StartElement {
-                    name: GenericXmlName { local_name: self.decode(bytes_start.local_name()).to_string() },
+                    name: GenericXmlName { local_name: self.decode(bytes_start.local_name()).unwrap().to_string() },
                     attributes: bytes_start.attributes()
                         .map(|x| x.unwrap())
                         .map(|x| GenericXmlAttribute {
-                            name: GenericXmlName { local_name: self.decode(x.key).to_string() },
+                            name: GenericXmlName { local_name: self.decode(x.key).unwrap().to_string() },
                             value: x.unescape_and_decode_value(self).unwrap(),
                         }).collect(),
                 }),
                 Event::End(bytes_end) => Ok(GenericXmlEvent::EndElement {
-                    name: GenericXmlName { local_name: self.decode(bytes_end.local_name()).to_string() },
+                    name: GenericXmlName { local_name: self.decode(bytes_end.local_name()).unwrap().to_string() },
                 }),
                 Event::Text(bytes_text) => {
                     let characters = bytes_text.unescape_and_decode(self).unwrap();
